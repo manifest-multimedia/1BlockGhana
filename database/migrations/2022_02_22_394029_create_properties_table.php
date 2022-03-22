dec5b4c976cbe944729341ff999f922a1b127409
Gabetech1 <gabrielabk1@gmail.com>
2022-03-22 06:11:29 +0000

    properties db updated

--- database/migrations/2022_02_22_394029_create_properties_table.php
+++ database/migrations/2022_02_22_394029_create_properties_table.php
@@ -21,7 +21,7 @@ class CreatePropertiesTable extends Migration
             $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->double('price', 8, 2);
-            $table->foreignId('currency_id')->nullable()->constrained('currencies')->onDelete('set null');
+            $table->foreignId('currency_id')->nullable()->constrained('currencies')->onDelete('cascade');
             $table->string('size')->nullable();
             $table->string('bedroom')->nullable();
             $table->string('kitchen')->nullable();
