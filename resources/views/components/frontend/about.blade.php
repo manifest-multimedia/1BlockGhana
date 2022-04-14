3338d7bbd5b31f6875de7545f0f681eca3305846
Gabetech1 <gabrielabk1@gmail.com>
2022-04-14 20:55:39 +0000

    about page updated

--- resources/views/components/frontend/about.blade.php
+++ resources/views/components/frontend/about.blade.php
@@ -14,8 +14,8 @@
                             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commodo consequat.
                         </p> --}}
-                        <a href="#" class="mt-3 btn btn-primary text-capitalize"> read more
-                            <i class="ml-3 fa fa-angle-right "></i></a>
+                        {{-- <a href="#" class="mt-3 btn btn-primary text-capitalize"> read more
+                            <i class="ml-3 fa fa-angle-right "></i></a> --}}
                     </div>
                 </div>
                 <div class="col-lg-6">
@@ -33,6 +33,87 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="home__about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <div class="card__image card__box-v1">
+                        {{-- <div class="card__image-header h-250 img-space">
+                            <div class="ribbon text-capitalize">sold out</div>
+                            <img src="frontend/images/gallery10.jpg" alt=""
+                                class="img-fluid w100 img-transition">
+                            <div class="info"> for sale</div>
+                        </div> --}}
+                        <div class="card__image-body">
+                            {{-- <span
+                                class="badge badge-primary text-capitalize mb-2">house</span> --}}
+                            <h6 class="text-capitalize mb-4">
+                                <i class="fa fa-star"></i>
+                                <a href="javascript:void(0)">Genuity</a>
+                            </h6>
+
+                            <p class="text-capitalize">
+
+                                Established to help promote genuine real estate companies, we also look to ensure that patrons are well protected from fraudsters and scammers.All listings on the site are vetted by our team to ensure that they come from a registered real estate entity.
+                            </p>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card__image card__box-v1">
+                        {{-- <div class="card__image-header h-250 img-space">
+                            <div class="ribbon text-capitalize">sold out</div>
+                            <img src="frontend/images/gallery10.jpg" alt=""
+                                class="img-fluid w100 img-transition">
+                            <div class="info"> for sale</div>
+                        </div> --}}
+                        <div class="card__image-body">
+                            {{-- <span
+                                class="badge badge-primary text-capitalize mb-2">house</span> --}}
+                            <h6 class="text-capitalize mb-4">
+                                <i class="fa fa-star"></i>
+                                <a href="javascript:void(0)">Reliability</a>
+                            </h6>
+
+                            <p class="text-capitalize">
+
+                                Get connected promptly and experience the best of Ghanaian hospitality by the leading real estate companies in serene and secured settlements all over the country.
+                            </p>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card__image card__box-v1">
+                        {{-- <div class="card__image-header h-250 img-space">
+                            <div class="ribbon text-capitalize">sold out</div>
+                            <img src="frontend/images/gallery10.jpg" alt=""
+                                class="img-fluid w100 img-transition">
+                            <div class="info"> for sale</div>
+                        </div> --}}
+                        <div class="card__image-body">
+                            {{-- <span
+                                class="badge badge-primary text-capitalize mb-2">house</span> --}}
+                            <h6 class="text-capitalize mb-4">
+                                <i class="fa fa-star"></i>
+                                <a href="javascript:void(0)">Secured</a>
+                            </h6>
+
+                            <p class="text-capitalize">
+                                Acquiring a property does not have to be scary.Protect yourself and your investment by speaking to professionals who will ensure you do not fall prey to fraudsters. You can choose to speak to any of our affiliate advisors on the best way to acquire property in Ghana.
+                            </p>
+
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
